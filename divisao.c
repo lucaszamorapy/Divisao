f29ab1e864627393b29540030d4af5a2c48513a8
Lucas Zamora <113631100+lucaszamorapy@users.noreply.github.com>
2022-10-24 22:33:42 -0300

    add files via upload

---
+++ divisao.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int a,b,divisao;
+    
+    printf("Digite o valor de b:\n");
+    scanf("%d",&a);
+    printf("Digite o valor de a:\n");
+    scanf("%d",&b);
+    
+    divisao = b/a;
+    
+    printf("A divisao dos dois numeros e: %d\n",divisao);
+}
+
+
